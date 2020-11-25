95dee7bd86e13c8dbc7cc8bee29185c3eb093a7c
James Foster <github@jgfoster.net>
2020-11-25 13:57:06 -0800

    Changes to get tests to pass (#3)
    * Files need to be at the top level because this library is an older version.
    * Changes to get tests to run.

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 /*
 bundle config --local path vendor/bundle
 bundle install
-bundle exec arduino_ci_remote.rb  --skip-compilation
+bundle exec arduino_ci.rb  --skip-examples-compilation
  */
 #include "Arduino.h"
 #include "ArduinoUnitTests.h"
